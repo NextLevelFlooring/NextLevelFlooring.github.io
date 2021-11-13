a6b693796e224a1057ef1cae873f955d507fb983
ar00n <asimpole.as@gmail.com>
2021-11-13 20:16:54 +0000

    gal

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-    $('.gallery ul li a').click(function() {
+    $('.gallery .container .row .col-sm a').click(function() {
         var itemID = $(this).attr('href');
         $('.gallery ul').addClass('item_open');
         $(itemID).addClass('item_open');
@@ -10,7 +10,7 @@ $( document ).ready(function() {
         return false;
     });
 
-    $(".gallery ul li a").click(function() {
+    $(".gallery .container .row .col-sm a").click(function() {
         $('html, body').animate({
             scrollTop: parseInt($("#top").offset().top) - $(".navbar").outerHeight()
         }, 400);
